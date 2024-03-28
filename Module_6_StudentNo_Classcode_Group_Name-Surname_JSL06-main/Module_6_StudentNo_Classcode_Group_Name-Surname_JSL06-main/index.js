6b73c97300435d8726087e682e9dea93f81f4624
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-03-28 14:18:41 +0200

    commit

--- Module_6_StudentNo_Classcode_Group_Name-Surname_JSL06-main/Module_6_StudentNo_Classcode_Group_Name-Surname_JSL06-main/index.js
+++ Module_6_StudentNo_Classcode_Group_Name-Surname_JSL06-main/Module_6_StudentNo_Classcode_Group_Name-Surname_JSL06-main/index.js
@@ -7,15 +7,17 @@ const menu = {
 
 // Function to display menu items by category
 function displayMenuItems(menu) {
+    const menuContainer = document.getElementById('menu')
     
     // Get the menu container element from the HTML
-    const menuContainer = document.getElementById('menu');
+    //const menuContainer = document.getElementById('menu');
 
     // Loop through each category and its items in the menu object
     for (const [category, items] of Object.entries(menu)) {
       const categoryElement = document.createElement('h3');
-      categoryElement.textContent =category;
+      categoryElement.textContent = category;
       menuContainer.appendChild(categoryElement);  
+      const listElement = document.createElement('ul');
 
 
       //Looping throuhg items in the category
@@ -26,12 +28,12 @@ function displayMenuItems(menu) {
 
         //Adding click event listener to each item to trigger addToOrder function
         listItem.addEventListener('click', () => addToOrder(item));
-        HTMLDataListElement.appendChild(listItem);
+        listElement.appendChild(listItem);
       });
       menuContainer.appendChild(listElement);
     }
 
-    }
+}
 
     //Function to add an item to the order list
     function addToOrder(itemName) {
@@ -44,6 +46,13 @@ function displayMenuItems(menu) {
         const newTotal = (currentTotal + itemPrice).toFixed(2);
         orderTotalElem.textContent = newTotal;
     }
+
+    //Function to initialise the menu system
+    function initMenuSystem() {
+        displayMenuItems(menu);
+    }
+
+    initMenuSystem();
 
         // Create an element to represent the category
 
@@ -66,10 +75,10 @@ function displayMenuItems(menu) {
             // Append the list item to the list of items
 
             
-}
+
 
 // Callback function for adding an item to the order
-function addToOrder(itemName) {
+//function addToOrder(itemName) {
     // Get the order items list and the order total element from the HTML
 
     // Create a list item for the order
@@ -81,12 +90,12 @@ function addToOrder(itemName) {
     // Calculate and update the total price
 
     // Update the text content of the order total element with the new total
-}
+//}
 
 // Function to initialize the menu system
-function initMenuSystem(menu) {
+//function initMenuSystem(menu) {
     // Call the function to display menu items
-}
+//}
 
 // Start the menu system by calling the init function
-initMenuSystem(menu);
+//initMenuSystem(menu);
